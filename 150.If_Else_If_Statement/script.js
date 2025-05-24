694cd5cdc7af1afc394003643fcd4dd0160b6fdb
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-05-24 14:25:08 +0530

    Add files via upload

---
+++ 150.If_Else_If_Statement/script.js
@@ -0,0 +1,12 @@
+console.log("Before if block.");
+let color = "green";
+if (color == "red") {
+  console.log("Stop");
+} else if (color == "yellow") {
+  console.log("Slow Down");
+} else if (color == "green") {
+  console.log("Go");
+} else {
+  console.log("Invalid color.");
+}
+console.log("After if block.");
