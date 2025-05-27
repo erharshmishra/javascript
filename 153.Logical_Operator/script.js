7bfa1508a5ff58591fd72da58586398b5151681b
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-05-27 18:43:59 +0530

    Add files via upload

---
+++ 153.Logical_Operator/script.js
@@ -0,0 +1,10 @@
+let marks = 35;
+if (marks >= 33 && marks <= 100) {
+  console.log("Pass");
+}
+if (marks >= 0 || marks <= 100) {
+  console.log("Fail");
+}
+if (!(marks >= 36)) {
+  console.log("Fail");
+}
