3322b9c270911a3cce149424cbbecf3104ad3447
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-05-28 19:22:39 +0530

    Add files via upload

---
+++ 155.Switch_Statement/script.js
@@ -0,0 +1,14 @@
+let color = "yellow";
+switch (color) {
+  case "red":
+    console.log("stop");
+    break;
+  case "yellow":
+    console.log("slow down");
+    break;
+  case "green":
+    console.log("go");
+    break;
+  default:
+    console.log("broken light");
+}
