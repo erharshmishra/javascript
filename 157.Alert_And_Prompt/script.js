2c04a3617f199e5a4a2e8f76d6bb68a2c3223da1
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-05-30 18:58:25 +0530

    Add files via upload

---
+++ 157.Alert_And_Prompt/script.js
@@ -0,0 +1,7 @@
+let firstName, lastName, fullName;
+firstName = prompt("Enter your first name : ");
+lastName = prompt("Enter your last name : ");
+fullName = `Welcome ${firstName} ${lastName}!`;
+alert(fullName);
+confirm("Can we print your name on display");
+console.log(fullName);
