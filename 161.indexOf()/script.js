6824946c077cd96ef1bb18d9d210ca0b3e177035
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-06-02 17:44:51 +0530

    Add files via upload

---
+++ 161.indexOf()/script.js
@@ -0,0 +1,7 @@
+let str = "ILoveCoding";
+console.log(str.indexOf("Love"));
+console.log(str.indexOf("J"));
+console.log(str.indexOf("o"));
+console.log(str.length);
+console.log(str[0]);
+console.log(str[str.length - 1]);
