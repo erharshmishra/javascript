a61ba72fca501be954d61d595c215168086b3f37
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-06-05 00:07:59 +0530

    Add files via upload

---
+++ 164.replace()_And_repeat()/script.js
@@ -0,0 +1,7 @@
+let str = "ILoveCoding";
+console.log(str.replace("Love", "do"));
+console.log(str.replace("ove", ""));
+console.log(str.replace("o", "x"));
+
+let msg = "Mishra ";
+console.log(msg.repeat(2));
