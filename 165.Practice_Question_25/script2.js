af259ca3bf7e28ed262addc758196f986c455994
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-06-06 23:04:57 +0530

    Add files via upload

---
+++ 165.Practice_Question_25/script2.js
@@ -0,0 +1,4 @@
+let Name = "ApnaCollege";
+console.log(Name.slice(4, 9));
+console.log(Name.indexOf("na"));
+console.log(Name.replace("Apna", "Our"));
---
+++ 165.Practice_Question_25/script3.js
@@ -0,0 +1,2 @@
+let name = "ApnaCollege";
+console.log(name.slice(4).replaceAll("l", "t"));
