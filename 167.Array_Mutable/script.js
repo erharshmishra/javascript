0f4e2ba9d2cb035bd7fc6d8216c5b5399584aa05
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-06-08 00:31:08 +0530

    Add files via upload

---
+++ 167.Array_Mutable/script.js
@@ -0,0 +1,7 @@
+let fruits = ["mango", "orange", "litchi"];
+console.log(fruits);
+fruits[0] = "banana";
+console.log(fruits);
+fruits[10] = "pineapple";
+console.log(fruits);
+console.log(fruits.length);
