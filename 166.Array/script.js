11676a3a4fcc64454f5d78635075c393c10b3ead
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-06-07 15:23:12 +0530

    Add files via upload

---
+++ 166.Array/script.js
@@ -0,0 +1,14 @@
+let students = ["harsh", "rohan", "ayush"];
+console.log(students);
+console.log(students.length);
+console.log(students[0][1]);
+console.log(students[0].length);
+
+let marks = [21, 34, 65, 96];
+console.log(marks);
+
+let mix = [21, 34, "harsh", 96, 3.5, "h"];
+console.log(mix);
+
+let empty = [];
+console.log(empty);
