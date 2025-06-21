289b60bdf942bba8fdc91dbe7c980dbf949fc6a0
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-06-21 21:00:16 +0530

    Add files via upload

---
+++ 177.slice()/script.js
@@ -0,0 +1,6 @@
+let colors = ["red", "yellow", "orange", "blue", "green"];
+console.log(colors.slice());
+console.log(colors.slice(2));
+console.log(colors.slice(-2));
+console.log(colors.slice(2, 4));
+console.log(colors.slice(2, colors.length));
