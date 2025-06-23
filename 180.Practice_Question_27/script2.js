da87e3c644304dd28701a77402c5e0068976c16a
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-06-23 13:26:41 +0530

    Add files via upload

---
+++ 180.Practice_Question_27/script2.js
@@ -0,0 +1,12 @@
+let languages = [
+  "c",
+  "c++",
+  "html",
+  "javascript",
+  "python",
+  "java",
+  "c#",
+  "sql",
+];
+console.log(languages.indexOf("javascript"));
+console.log(languages.reverse().indexOf("javascript"));
