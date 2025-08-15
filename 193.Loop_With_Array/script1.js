ecced3606e41b977140c2130b4239ecb839278ab
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-08-15 14:59:32 +0530

    Add files via upload

---
+++ 193.Loop_With_Array/script1.js
@@ -0,0 +1,5 @@
+let fruits = ["Mango", "Apple", "Pineapple", "Orange", "Litchi"];
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(i, fruits[i]);
+}
---
+++ 193.Loop_With_Array/script2.js
@@ -0,0 +1,5 @@
+let fruits = ["Mango", "Apple", "Pineapple", "Orange", "Litchi"];
+
+for (let i = fruits.length - 1; i >= 0; i--) {
+  console.log(i, fruits[i]);
+}
