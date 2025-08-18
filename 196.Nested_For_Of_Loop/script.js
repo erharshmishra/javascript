04577469013c6382b49b0bdb0abca3026dad68e6
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-08-18 12:04:43 +0530

    Add files via upload

---
+++ 196.Nested_For_Of_Loop/script.js
@@ -0,0 +1,10 @@
+let movies = [
+  ["spiderman", "superman", "thor"],
+  ["ironman", "wonder woman", "flash"],
+];
+
+for (let list of movies) {
+  for (let movie of list) {
+    console.log(movie);
+  }
+}
