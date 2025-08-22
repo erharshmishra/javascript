a44235ee1233e26d9e83a0d50ee2b2c6836c8933
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-08-22 20:40:29 +0530

    Add files via upload

---
+++ 200.Practice_Question_32/script.js
@@ -0,0 +1,8 @@
+let post = {
+  username: "@akaharsh",
+  content: "I am bad boy.",
+  likes: 200000,
+  reposts: 1000,
+  tags: ["@erharshmishra", "@apnacollege", "@akaharsh"],
+};
+console.log(post);
