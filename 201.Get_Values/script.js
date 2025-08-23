ed122f6416a22f908b58bf9b92dfb575ccbc3070
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-08-23 18:08:45 +0530

    Add files via upload

---
+++ 201.Get_Values/script.js
@@ -0,0 +1,10 @@
+let post = {
+  username: "@akaharsh",
+  content: "I am bad boy.",
+  likes: 200000,
+  reposts: 1000,
+  tags: ["@erharshmishra", "@apnacollege", "@akaharsh"],
+};
+console.log(post);
+console.log(post.content);
+console.log(post["content"]);
