bca09a051556e740efa1242e8ba3a221073eebd7
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-08-26 19:17:08 +0530

    Add files via upload

---
+++ 204.Array_Of_Objects/script.js
@@ -0,0 +1,19 @@
+const classInfo = [
+  {
+    name: "Aman",
+    grade: "A+",
+    city: "Delhi",
+  },
+  {
+    name: "Harsh",
+    grade: "O",
+    city: "Gorakhpur",
+  },
+  {
+    name: "Karan",
+    grade: "A",
+    city: "Pune",
+  },
+];
+console.log(classInfo);
+console.log(classInfo[0].grade);
