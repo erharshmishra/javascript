29ba34aa6435612c74d9aab141e52c54883bca7c
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-09-02 19:49:24 +0530

    Add files via upload

---
+++ 211.Practice_Question_36/script1.js
@@ -0,0 +1,6 @@
+function poem() {
+  console.log(
+    "Johny, Johny\nYes, Papa?\nEating sugar?\nNo, Papa.\nTelling lies?\nNo, Papa.\nOpen your mouth.\nHa-ha-ha!"
+  );
+}
+poem();
---
+++ 211.Practice_Question_36/script2.js
@@ -0,0 +1,5 @@
+function roll() {
+  let dice = Math.floor(Math.random() * 6) + 1;
+  console.log(dice);
+}
+roll();
