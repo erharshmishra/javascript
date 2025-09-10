920691f2e7ec3dd11fd9eb621f42ebe0232d9b3a
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-09-10 21:17:15 +0530

    Add files via upload

---
+++ 218.Higher_Order_Function/script1.js
@@ -0,0 +1,10 @@
+function multipleGreet(func, n) {
+  for (let i = 1; i <= n; i++) {
+    func();
+  }
+}
+let greet = function () {
+  console.log("hello");
+};
+
+multipleGreet(greet, 2);
---
+++ 218.Higher_Order_Function/script2.js
@@ -0,0 +1,16 @@
+function oddEvenTest(request) {
+  if (request == "odd") {
+    return function (n) {
+      console.log(!(n % 2 == 0));
+    };
+  } else if (request == "even") {
+    return function (n) {
+      console.log(n % 2 == 0);
+    };
+  } else {
+    console.log("Wrong Request.");
+  }
+}
+
+let func = oddEvenTest("odd");
+func(21);
