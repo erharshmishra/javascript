61e4d3f3b5593365a8d115f024e4c7183040699a
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-09-14 21:55:34 +0530

    Add files via upload

---
+++ 222.Try_&_Catch/script.js
@@ -0,0 +1,14 @@
+console.log("hello1");
+console.log("hello1");
+
+try {
+  console.log(a);
+} catch {
+  console.log("variable a is not defined");
+} finally {
+  console.log("all the best");
+}
+
+console.log("hello2");
+console.log("hello2");
+console.log("hello2");
