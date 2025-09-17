fd2d4b840d80f93a38a6e464755f0f30fa60ddd6
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-09-18 00:24:46 +0530

    Add files via upload

---
+++ 226.Set_Interval_Function/script.js
@@ -0,0 +1,10 @@
+setInterval(() => {
+  console.log("Harsh Mishra");
+}, 1000);
+
+let id = setInterval(() => {
+  console.log("Harsh");
+}, 1000);
+
+// Use to stop the execution.
+clearInterval(id);
