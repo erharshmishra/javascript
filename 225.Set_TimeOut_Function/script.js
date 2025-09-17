cba47f76374cbb7734178d66db9e3dc8718b033c
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-09-17 23:11:55 +0530

    Add files via upload

---
+++ 225.Set_TimeOut_Function/script.js
@@ -0,0 +1,23 @@
+// console.log("Hi,there!");
+// setTimeout(function sum() {
+//     console.log("Apna College.");
+// }, 1000);
+// console.log("Welcome to,");
+
+// console.log("Hi,there!");
+// setTimeout( sum = function() {
+//     console.log("Apna College.");
+// }, 1000);
+// console.log("Welcome to,");
+
+// console.log("Hi,there!");
+// setTimeout( sum = () => {
+//     console.log("Apna College.");
+// }, 1000);
+// console.log("Welcome to,");
+
+console.log("Hi,there!");
+setTimeout(() => {
+  console.log("Apna College.");
+}, 1000);
+console.log("Welcome to,");
