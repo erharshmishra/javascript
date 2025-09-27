30d742192efbf9355e006ec95e02234816b0b8f5
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-09-27 22:33:51 +0530

    Add files via upload

---
+++ 234.some()/script.js
@@ -0,0 +1,20 @@
+let arr = [2, 3, 5, 6, 8, 9];
+
+let trueOrFalse = arr.some((ele) => {
+  return ele % 2 == 0;
+});
+console.log(trueOrFalse);
+
+let array = [2, 4, 6];
+
+let newtrueOrFalse = array.some((ele) => {
+  return ele % 2 == 0;
+});
+console.log(newtrueOrFalse);
+
+let ar = [1, 3, 5];
+
+let newar = ar.some((ele) => {
+  return ele % 2 == 0;
+});
+console.log(newar);
