088c859edf02517d553328b0cf61e1c964d03565
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-10-11 21:34:31 +0530

    Add files via upload

---
+++ 239.Spread_Array_Literal/script.js
@@ -0,0 +1,11 @@
+let arr = [1, 2, 3, 4, 5];
+let newArr = [...arr];
+console.log(newArr);
+
+let chars = [..."hello"];
+console.log(chars);
+
+let even = [2, 4, 6, 8, 10];
+let odd = [1, 3, 5, 7, 9];
+let nums = [...even, ...odd];
+console.log(nums);
