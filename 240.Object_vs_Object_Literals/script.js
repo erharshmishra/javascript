16ea7558c5129f70da72ed23c9c32cb908785d84
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-10-11 21:34:43 +0530

    Add files via upload

---
+++ 240.Object_vs_Object_Literals/script.js
@@ -0,0 +1,17 @@
+// Object Literals '{}' identity.
+
+let car = {
+  model: "swift desire 2024 model",
+  color: "black",
+  type: "car",
+};
+console.log(car);
+
+// Object.
+
+let fruits = new Object();
+fruits.fr1 = "mango";
+fruits.fr2 = "orange";
+fruits.fr3 = "banana";
+fruits.fr4 = "pineapple";
+console.log(fruits);
