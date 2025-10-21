b54208f2218048f9fd75cbdadfb94e36c19d0eed
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-10-21 23:32:48 +0530

    Add files via upload

---
+++ 247.Selecting_Element/script.js
@@ -0,0 +1,22 @@
+// Select Elemnt by Id.
+let a = document.getElementById("mainImg");
+console.log(a);
+// a.src = "assets/creation_1.png";
+
+// Select Elemnt by Class Name.
+let b = document.getElementsByClassName("oldImg");
+console.log(b);
+console.log(b[2]);
+
+// const d = () => {
+//     a.src = "assets/creation_1.png";
+// }
+
+// Select Elemnt by Tag Name.
+let c = document.getElementsByTagName("p");
+console.log(c);
+console.dir(c);
+
+// let e = document.getElementsByTagName("h1");
+// console.log(e);
+// e[0].innerText = "Harsh Mishra";
