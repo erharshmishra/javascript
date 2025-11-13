4935c86aa18633beeede635d74986932c3c71233
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-11-13 22:36:25 +0530

    Add files via upload

---
+++ 248.Query_Selector/script.js
@@ -0,0 +1,11 @@
+// Select Element by Query Selector.
+
+console.log(document.querySelector("p"));
+console.log(document.querySelector("#description"));
+console.log(document.querySelector(".oldImg"));
+
+// Select Element by Query Selector All.
+console.log(document.querySelectorAll(".oldImg"));
+console.log(document.querySelectorAll("p"));
+console.log(document.querySelectorAll("a"));
+console.log(document.querySelectorAll("div a"));
