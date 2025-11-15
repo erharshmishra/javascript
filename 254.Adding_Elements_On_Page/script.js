a45b21ee84c955c6605f6cbd524764f9c488d008
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-11-15 21:27:37 +0530

    Add files via upload

---
+++ 254.Adding_Elements_On_Page/script.js
@@ -0,0 +1,30 @@
+let newP = document.createElement("p");
+console.log(newP);
+newP.innerText = "Hi, I am a new Paragraph.";
+console.log(newP);
+
+let body = document.querySelector("body");
+body.appendChild(newP);
+
+newP.append(
+  "After ( Hi, I am a new Paragraph.) Now i am trying to add this text in my previous newP"
+);
+console.log(newP);
+newP.prepend(
+  "Before ( Hi, I am a new Paragraph.) Now i am trying to add this text in my previous newP"
+);
+console.log(newP);
+
+let box = document.querySelector(".box");
+let btn = document.createElement("button");
+let btn1 = document.createElement("button");
+btn1.innerText = "Don't Click Me";
+btn.innerText = "Click Me";
+console.log(btn1);
+box.append(btn1);
+box.append(btn);
+
+// box.insertAdjacentElement("beforebegin",btn)
+// box.insertAdjacentElement("afterbegin",btn)
+// box.insertAdjacentElement("beforeend",btn)
+box.insertAdjacentElement("afterend", btn);
