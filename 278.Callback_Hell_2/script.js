24f942077b04b93df9a86732126d518ea83dcf4f
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-12-07 22:53:22 +0530

    Add files via upload

---
+++ 278.Callback_Hell_2/script.js
@@ -0,0 +1,37 @@
+function savetoDB(data, success, failure) {
+  let internetSpeed = Math.floor(Math.random() * 10) + 1;
+  if (internetSpeed > 4) {
+    success();
+  } else {
+    failure();
+  }
+}
+savetoDB(
+  "harshmishra",
+  () => {
+    console.log("Success1 : Data saved");
+    savetoDB(
+      "apnacolloge",
+      () => {
+        console.log("Success2 : Data saved");
+        savetoDB(
+          "helloworld",
+          () => {
+            console.log("Success3 : Data saved");
+          },
+          () => {
+            console.log("Failure3 : Weak connection");
+          }
+        );
+      },
+      () => {
+        console.log("Failure2 : Weak connection");
+      }
+    );
+  },
+  () => {
+    console.log("Failure1 : Weak connection");
+  }
+);
+
+// Callbacks Nesting ==> Callback Hell.
