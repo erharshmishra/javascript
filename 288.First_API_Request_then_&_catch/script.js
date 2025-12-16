7c1d064416497d5a1d3221f13e92123358ff8ea1
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-12-16 12:10:18 +0530

    Add files via upload

---
+++ 288.First_API_Request_then_&_catch/script.js
@@ -0,0 +1,23 @@
+// let url = "https://catfact.ninja/fact";
+// fetch(url);
+
+let url = "https://catfact.ninja/fact";
+
+fetch(url)
+  .then((res) => {
+    // console.log(res);
+    return res.json();
+  })
+  .then((data) => {
+    console.log("Data1 : ", data.fact);
+    return fetch(url);
+  })
+  .then((res) => {
+    return res.json();
+  })
+  .then((data2) => {
+    console.log("Data2 : ", data2.fact);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
